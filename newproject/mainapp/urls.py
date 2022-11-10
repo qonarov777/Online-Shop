0aed51931454a0b4f7cbeedb7041fd9d82acefde
shohrux0113 <qonarov777@gmail.com>
2022-11-10 23:52:40 +0500

    commit

--- newproject/mainapp/urls.py
+++ newproject/mainapp/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from .views import IndexView
+from .views import ShopView
+from .views import DetailView
+from .views import ContactView
+from .views import CheckoutView
+from .views import CartView
 
 urlpatterns = [
-    path('', IndexView),
+    path('index/', IndexView),
+    path('shop/', ShopView),
+    path('detail/', DetailView),
+    path('contact/', ContactView),
+    path('checkout/',CheckoutView),
+    path('cart/',CartView),
 ]
--- newproject/mainapp/views.py
+++ newproject/mainapp/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def IndexView(request):
     return render(request, 'index.html')
+
+def ShopView(request):
+    return render(request, 'shop.html')
+
+def DetailView(request):
+    return render(request, 'detail.html')
+
+def ContactView(request):
+    return render(request, 'contact.html')
+
+def CheckoutView(request):
+    return render(request, 'checkout.html')
+
+def CartView(request):
+    return render(request, 'cart.html')
