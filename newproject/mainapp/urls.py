ad208e466c56fff5eeb643bcc53c594f824d5bc9
shohrux0113 <qonarov777@gmail.com>
2022-11-12 23:59:08 +0500

    ss commit

--- newproject/mainapp/urls.py
+++ newproject/mainapp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .views import IndexView
-# from .views import ShopView
-# from .views import DetailView
-# from .views import ContactView
-# from .views import CheckoutView
-# from .views import CartView
+from .views import ShopView
+from .views import DetailView
+from .views import ContactView
+from .views import CheckoutView
+from .views import CartView
 
 urlpatterns = [
-    path('', IndexView),
-    # path('shop/', ShopView),
-    # path('detail/', DetailView),
-    # path('contact/', ContactView),
-    # path('checkout/',CheckoutView),
-    # path('cart/',CartView),
+    path('', IndexView, name='IndexView'),
+    path('shop/', ShopView, name='ShopView'),
+    path('detail/', DetailView, name='DetailView'),
+    path('contact/', ContactView, name='ContactView'),
+    path('checkout/',CheckoutView, name='CheckoutView'),
+    path('cart/',CartView, name='CartView'),
 ]
--- newproject/mainapp/views.py
+++ newproject/mainapp/views.py
@@ -10,17 +10,17 @@ def IndexView(request):
     
     return render(request, 'index.html', context=context)
 
-# def ShopView(request):
-#     return render(request, 'shop.html')
+def ShopView(request):
+    return render(request, 'shop.html')
 
-# def DetailView(request):
-#     return render(request, 'detail.html')
+def DetailView(request):
+    return render(request, 'detail.html')
 
-# def ContactView(request):
-#     return render(request, 'contact.html')
+def ContactView(request):
+    return render(request, 'contact.html')
 
-# def CheckoutView(request):
-#     return render(request, 'checkout.html')
+def CheckoutView(request):
+    return render(request, 'checkout.html')
 
-# def CartView(request):
-#     return render(request, 'cart.html')
+def CartView(request):
+    return render(request, 'cart.html')
